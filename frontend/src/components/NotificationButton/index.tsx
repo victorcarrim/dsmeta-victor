d7ecaf51b45deab3e942a3d167545646e552a57e
Victor Ferrari <victorfonsecaferrari@gmail.com>
2022-07-11 14:00:48 -0300

    First component

---
+++ frontend/src/components/NotificationButton/index.tsx
@@ -0,0 +1,14 @@
+import icon from '../../assets/img/notification-icon.svg'
+import './styles.css'
+
+function NotificationButton() {
+    return (
+        <div className="dsmeta-red-btn-container">
+            <div className="dsmeta-red-btn">
+                <img src={icon} alt="notificar" />
+            </div>
+        </div>
+    )
+}
+
+export default NotificationButton;
